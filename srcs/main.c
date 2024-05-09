c2cac4fbfe8dd99c022830dd4265250370ea5033
Omar Felk <omfelk@c2r5p10.42perpignan.fr>
2024-05-09 14:36:47 +0200

    gere le fork

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: omfelk <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 15:07:58 by omfelk            #+#    #+#             */
-/*   Updated: 2024/05/08 19:44:50 by omfelk           ###   ########.fr       */
+/*   Updated: 2024/05/09 12:25:18 by omfelk           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,10 @@
 // valgrind --leak-check=full --show-leak-kinds=all
 //  --suppressions=val.supp -s  ./minishell
 
-static bool	ex_child(char *str_traitement, t_data *lst_data, int	*pipe_fd)
+static bool	ex_child(char *str_traitement, t_data *lst_data, int *pipe_fd)
 {
  	(void)lst_data;
+	(void)str_traitement;
 
  	close(pipe_fd[0]);
  	if (dup2(pipe_fd[1], STDOUT_FILENO) == -1)
@@ -27,15 +28,14 @@ static bool	ex_child(char *str_traitement, t_data *lst_data, int	*pipe_fd)
  	}
  	close(pipe_fd[1]);
 	gest_readline_recover(str_traitement, lst_data);
-	printf("child\n");
+	// printf("child\n");
+	// printf("strr = %s\n", str_traitement);
  	return (true);
 }
 
 bool	ex_father(t_data *lst_data, int *pipe_fd, int child_pid)
 {
- 	(void)lst_data;
-	char	*tmp;
-
+	(void)lst_data;
 
 	close(pipe_fd[1]);
 	if (dup2(pipe_fd[0], STDIN_FILENO) == -1)
@@ -45,19 +45,19 @@ bool	ex_father(t_data *lst_data, int *pipe_fd, int child_pid)
  	}
  	close(pipe_fd[0]);
  	waitpid(child_pid, NULL, 0);
-	tmp = get_next_line(STDIN_FILENO);
-	printf("recupe = %s\n", tmp);
-	free(tmp);
-	printf("father\n");
+	// printf("father\n");
 	return (true);
 }
 
 static	bool	ft_ex(char *str_traitement, t_data *lst_data)
 {
+	(void)lst_data;
  	pid_t	child_pid;
  	int		pipe_fd[2];
+	char	*tmp;
 
-	printf("manager\n");
+	// printf("manager\n");
+	// printf("str = %s\n", str_traitement);
 	if (pipe(pipe_fd) == -1)
 		perror("error pipe ");
  	child_pid = fork();
@@ -70,6 +70,9 @@ static	bool	ft_ex(char *str_traitement, t_data *lst_data)
  		ex_child(str_traitement, lst_data, pipe_fd);
  	else
  		ex_father(lst_data, pipe_fd, child_pid);
+	tmp = get_next_line(STDIN_FILENO);
+	printf("recupe = %s\n", tmp);
+	free(tmp);
 	return (true);
 }
 
@@ -79,19 +82,20 @@ static	bool	ft_manager(t_data *lst_data)
 	char	**tab_arm_pipe;
 	int		i;
 
-	while (1)
-	{
+	// while (1)
+	// {
 		i = -1;
 		readlin_recover = add_signal_plus_return_result_prompt(lst_data);
 		tab_arm_pipe = ft_split(readlin_recover, '|');
-		while (tab_arm_pipe[i++])
+		while (tab_arm_pipe[++i])
 		{
 			tab_arm_pipe[i] = parsing(tab_arm_pipe[i], lst_data);
 			if (tab_arm_pipe[i])
 				ft_ex(tab_arm_pipe[i], lst_data);
 		}
 		my_free_tab(tab_arm_pipe);
-	}
+		free(readlin_recover);
+	// }
 	return (true);
 }
 
